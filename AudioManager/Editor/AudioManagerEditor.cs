8d798589a086ef141a776f82988f9e3373b23c0a
Jacky Yang <jackyyang267@gmail.com>
2019-07-02 23:47:22 -0400

    Add files via upload

---
+++ AudioManager/Editor/AudioManagerEditor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+/// <summary>
+/// Thank god to brownboot67 for his advice
+/// https://forum.unity.com/threads/custom-editor-not-saving-changes.424675/
+/// </summary>
+[CustomEditor(typeof(AudioManager))]
+public class AudioManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        AudioManager myScript = (AudioManager)target;
+
+        string editorMessage = myScript.GetEditorMessage();
+        if (editorMessage != "")
+        {
+            EditorGUILayout.HelpBox(editorMessage, MessageType.Info);
+        }
+
+        DrawDefaultInspector();
+
+        List<string> options = new List<string>();
+
+        options.Add("None");
+        foreach (string s in myScript.GetMusicDictionary().Keys)
+        {
+            options.Add(s);
+        }
+
+        GUIContent content = new GUIContent("Current Track", "Current music that's playing, will play on start if not \"None\"");
+
+        serializedObject.FindProperty("currentTrack").stringValue = options[EditorGUILayout.Popup(content, options.IndexOf(serializedObject.FindProperty("currentTrack").stringValue), options.ToArray())];
+
+        serializedObject.ApplyModifiedProperties();
+
+        if (myScript.GetSoundDictionary().Count > 0)
+        {
+            string list = "List of Sounds:";
+            foreach (string s in myScript.GetSoundDictionary().Keys)
+            {
+                list += "\n" + s;
+            }
+            EditorGUILayout.HelpBox(list, MessageType.None);
+        }
+
+        if (myScript.GetMusicDictionary().Count > 0)
+        {
+            string musiks = "List of Music:";
+            foreach (string m in myScript.GetMusicDictionary().Keys)
+            {
+                musiks += "\n" + m;
+            }
+            EditorGUILayout.HelpBox(musiks, MessageType.None);
+        }
+    }   
+}
---
+++ AudioManager/Editor/AudioParticlesEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AudioParticles))]
+public class AudioParticlesEditor : Editor
+{
+    AudioManager am;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (am == null) am = AudioManager.GetInstance();
+
+        AudioParticles myScript = (AudioParticles)target;
+        
+        List<string> options = new List<string>();
+
+        options.Add("None");
+        foreach (string s in am.GetSoundDictionary().Keys)
+        {
+            options.Add(s);
+        }
+
+        //if (AudioManager.GetInstance().GetMusicDictionary() == null)
+        //{
+        //    EditorGUILayout.HelpBox("Error! Add an audio file before running!", MessageType.Error);
+        //}
+
+        GUIContent soundDesc = new GUIContent("Sound", "Sound that will be played when particles spawn/die");
+
+        if (serializedObject.FindProperty("sound").stringValue.Equals("")) EditorGUILayout.Popup(soundDesc, 0, options.ToArray());
+        else serializedObject.FindProperty("sound").stringValue = options[EditorGUILayout.Popup(soundDesc, options.IndexOf(serializedObject.FindProperty("sound").stringValue), options.ToArray())];
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
