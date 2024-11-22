aa7e607f9f84fb0a237e140830e55ef77d8f4ff4
Jacky Yang <jackyyang267@gmail.com>
2024-11-22 13:26:49 -0500

    Finished implementation of AudioLibraryLoader

--- Runtime/Scripts/AudioLibraryLoader.cs
+++ Runtime/Scripts/AudioLibraryLoader.cs
@@ -16,11 +16,30 @@ namespace JSAM
 
         public LoadBehaviour loadTiming = LoadBehaviour.OnStartAndDestroy;
 
+        void Load() => AudioManagerInternal.Instance.LoadAudioLibrary(library);
+        void Unload() => AudioManagerInternal.Instance.UnloadAudioLibrary(library);
+
+        private void OnEnable()
+        {
+            if (loadTiming == LoadBehaviour.OnEnableAndDisable)
+            {
+                Load();
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (loadTiming == LoadBehaviour.OnEnableAndDisable)
+            {
+                Unload();
+            }
+        }
+
         private void Start()
         {
             if (loadTiming == LoadBehaviour.OnStartAndDestroy)
             {
-                AudioManagerInternal.Instance.LoadAudioLibrary(library);
+                Load();
             }
         }
 
@@ -28,7 +47,7 @@ namespace JSAM
         {
             if (loadTiming == LoadBehaviour.OnStartAndDestroy)
             {
-                AudioManagerInternal.Instance.UnloadAudioLibrary(library);
+                Unload();
             }
         }
     }
